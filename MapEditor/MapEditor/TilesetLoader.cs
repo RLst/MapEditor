b7bc685bce1134e7a247e977929104511d379c59
Tony Le <letony@icloud.com>
2018-08-23 10:22:10 +1000

    Basic setup; Renames added Map and Tile classes

--- MapEditor/MapEditor/TilesetLoader.cs
+++ MapEditor/MapEditor/TilesetLoader.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace MapEditor
 {
-    public partial class Form1 : Form
+    public partial class tilesetLoader : Form
     {
-        public Form1()
+        public tilesetLoader()
         {
             InitializeComponent();
         }
