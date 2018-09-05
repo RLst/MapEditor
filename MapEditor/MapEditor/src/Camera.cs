5eceec11e8d56639fbf6709510eb2718a5753ba4
Tony Le <letony@icloud.com>
2018-09-05 15:16:31 +1000

    Camera init commit

---
+++ MapEditor/MapEditor/src/Camera.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapEditor
+{
+	public class Camera
+	{
+		//Position
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		//Zoom
+		public float Zoom { get; set; }
+
+	}
+}
